6a98ba9c77344777b069dd73b659a7deec910586
mamesaliou <mamesalioucisse@gmail.com>
2025-10-31 12:07:45 +0100

    update

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,14 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Utiliser SQLite pour les tests, PostgreSQL pour la production
-if os.getenv("TESTING", "False").lower() == "true":
-    DATABASE_URL = "sqlite:///./test.db"
-else:
-    DATABASE_URL = os.getenv(
-        "DATABASE_URL", 
-        "postgresql://user:password@db:5432/school_attendance"
-    )
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://user:password@db:5432/school_attendance"
+)
 
 engine = create_engine(
     DATABASE_URL,
@@ -26,10 +23,4 @@ def get_db():
     finally:
         db.close()
 
-# Importer tous les modèles pour qu'ils soient reconnus par SQLAlchemy
-# from ..models.models import Class, Student, Attendance
-# from ..auth.model import User
-
-# Créer les tables seulement si ce n'est pas un import de test
-if not os.getenv("TESTING"):
-    Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
