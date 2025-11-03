a68602a07dc80821f1c4c7ca490b6b603985dc0b
mamesaliou <mamesalioucisse@gmail.com>
2025-11-03 17:50:44 +0100

    add updated database.py and .env.example files

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DB")}'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
